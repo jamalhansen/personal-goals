d2e3d0b17a41910cd1046e7f47d96fa18c28ded2
Jamal Hansen <jamal.hansen@gmail.com>
2015-11-28 18:59:48 -0600

    Prepares reset.py script

--- reset.py
+++ reset.py
@@ -1,51 +1,22 @@
-from os.path import join
+#! /usr/bin/env python3
+
 from os import getcwd
 from py import path
 
-import pytest
 import py
 
 from pg_tools.weekly_update import WeeklyUpdate
 from pg_tools.goals import Goals
 
 
-def test_charlie_uses_quick_shortcut(tmpdir):
-    # Charlie wants an eaiser way that makes some assumptions.
-    current_dir = getcwd()
-    source_dir = join(current_dir, 'tests', 'fixtures')
-    goals = Goals(source_dir, tmpdir)  #not sure why this exists  Need to revist
+# Charlie wants an eaiser way that makes some assumptions.
+current_dir = getcwd()
 
-    source = "basic_list.md"
-    checked_out = tmpdir.join("target_checked.md")
-    unchecked_out = tmpdir.join("target_unchecked.md")
+source = "README.md"
+local = py.path.local(current_dir)
 
-    assert isinstance(checked_out, py.path.local)
+checked_out = local.join("README.md.checked")
+unchecked_out = local.join("README.md.new")
 
-    wu = WeeklyUpdate(source_dir, source, checked_out, unchecked_out)
-
-    # now he check his output
-    assert not wu.checked_target.check(exists=1)
-    assert not wu.unchecked_target.check(exists=1)
-    wu.wrap_up_week()
-
-    # and checks for the new file
-    assert wu.checked_target.check(exists=1)
-    assert wu.unchecked_target.check(exists=1)
-
-    # and that it contains 3 lines
-    text = wu.checked_target.read()
-
-    lines = text.split("\n")
-    assert len(lines) == 3
-
-    # and that the lines contain the correct text
-    for line in lines:
-        assert line[:5].upper() == "- [X]"
-        assert line[6:].upper().strip() == "CHECKED"
-
-    # and that the checked items are removed from the original file
-    text = wu.unchecked_target.read()
-
-    assert "Checked" not in text
-
-    # Satisfied she starts writing her blog post about her week
+wu = WeeklyUpdate(current_dir, source, checked_out, unchecked_out)
+wu.wrap_up_week()
